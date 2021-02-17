23c56303c1ba266ac876898a881be724931e495d
MJ Maun <Mikhaela.Maun@MytikasLLC.com>
2021-02-18 01:37:19 +0800

    added contact us component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { MaterialModule } from '../../shared/material/material.module';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { LayoutComponent } from './layout.component';
@@ -10,6 +11,7 @@ import { NewsComponent } from './landing-page/_components/news/news.component';
 import { SubscribeComponent } from './landing-page/_components/subscribe/subscribe.component';
 import { ServicesComponent } from './services/services.component';
 import { WhatCanIDoComponent } from './services/_components/what-can-i-do/what-can-i-do.component';
+import { ContactUsComponent } from './services/_components/contact-us/contact-us.component';
 
 
 
@@ -18,6 +20,7 @@ import { WhatCanIDoComponent } from './services/_components/what-can-i-do/what-c
     CommonModule,
     HomeRoutingModule,
     FormsModule,
+    // MaterialModule,
     ReactiveFormsModule
   ],
   declarations: [
@@ -28,6 +31,7 @@ import { WhatCanIDoComponent } from './services/_components/what-can-i-do/what-c
     SubscribeComponent,
     ServicesComponent,
     WhatCanIDoComponent,
+    ContactUsComponent,
   ],
   bootstrap: []
 })
