a3901318b304ac7f3d2ec1db74d4c0484aa9bd92
MJ Maun <Mikhaela.Maun@MytikasLLC.com>
2021-02-19 04:08:43 +0800

    updated news link

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
           { path: '', component: LandingPageComponent },
           { path: 'services', component: ServicesComponent },
           { path: 'listen', component: ListenToMyMusicComponent },
-          { path: 'latest-news', component: LatestNewsComponent },
+          { path: 'news', component: LatestNewsComponent },
       ]
   }
 ];
