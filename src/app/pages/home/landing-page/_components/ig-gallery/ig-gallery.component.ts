a65bdf7cd970c003fb751d6a12d57bbb50bf528a
Regina Lopez <regina.lopez@mytikasllc.com>
2021-02-18 23:49:52 +0800

    component ui fixes

--- src/app/pages/home/landing-page/_components/ig-gallery/ig-gallery.component.ts
+++ src/app/pages/home/landing-page/_components/ig-gallery/ig-gallery.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-ig-gallery',
+  selector: 'ig-gallery',
   templateUrl: './ig-gallery.component.html',
   styleUrls: ['./ig-gallery.component.less']
 })
