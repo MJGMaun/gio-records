37e6b966657223dd07f03f1a5a3d36ea1c8b8c33
MJ Maun <Mikhaela.Maun@MytikasLLC.com>
2021-03-08 23:34:45 +0800

    api ready

--- src/app/pages/home/landing-page/_components/news/news.component.ts
+++ src/app/pages/home/landing-page/_components/news/news.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiService } from 'src/app/_services';
 @Component({
   selector: 'news',
   templateUrl: './news.component.html',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+  recentNews: any = [];
+
+  constructor( public _api: ApiService ) { }
 
   ngOnInit(): void {
+    this.getNews();
+  }
+
+  getNews() {
+    this._api.get('/News/RecentNews.json').subscribe((res)=> {
+      console.log(res);
+      this.recentNews = res;
+    })
   }
 
 }
