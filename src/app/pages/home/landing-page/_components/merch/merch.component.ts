7e8bc75f18a6417be41e5cc1ddbbcc2d51909218
MJ Maun <Mikhaela.Maun@MytikasLLC.com>
2021-03-16 23:59:55 +0800

    removed carousel, dynamic data

--- src/app/pages/home/landing-page/_components/merch/merch.component.ts
+++ src/app/pages/home/landing-page/_components/merch/merch.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import {
   Image,
   LineLayout,
@@ -6,61 +7,28 @@ import {
   PlainGalleryStrategy
 } from '@ks89/angular-modal-gallery';
 
+import { ApiService } from 'src/app/_services';
+
 @Component({
   selector: 'merch',
   templateUrl: './merch.component.html',
   styleUrls: ['./merch.component.less']
 })
 export class MerchComponent implements OnInit {
-  imageIndex = 1;
-  galleryId = 1;
-  isPlaying = true;
+  merchList: any = [];
+  featuredMerch: any = [];
 
-  merchImages: Image[] = [
-    new Image(
-      0,
-      { // modal
-        img: '../../../../../../assets/images/merch-1.png',
-        description: 'Description 1'
-      }
-    ),
-    new Image(
-      1,
-      { // modal
-        img: '../../../../../../assets/images/merch-2.png',
-        description: 'Description 2'
-      }
-    ),
-    new Image(
-      3,
-      { // modal
-        img: '../../../../../../assets/images/merch-3.png',
-        description: 'Description 3'
-      }
-    ),
-    new Image(
-      4,
-      { // modal
-        img: '../../../../../../assets/images/merch-4.png',
-        description: 'Description 4'
-      }
-    )
-  ];
-
-
-  constructor() { }
+  constructor(
+    public _api: ApiService,
+  ) { }
 
   ngOnInit(): void {
+    this.getMerchList();
   }
 
-  singleImage: Image[] = [this.merchImages[0]];
-  singleImage2: Image[] = [this.merchImages[1]];
-  singleImage3: Image[] = [this.merchImages[2]];
-  singleImage4: Image[] = [this.merchImages[3]];
-
-  plainGalleryRow: PlainGalleryConfig = {
-    strategy: PlainGalleryStrategy.ROW,
-    layout: new LineLayout({ width: '100%', height: 'auto' }, { length: 1, wrap: true }, 'flex-start')
-  };
-
+  getMerchList() {
+    this._api.get('/Merch/FeaturedMerch.json').subscribe((res)=> {
+      this.featuredMerch = res;
+    });
+  }
 }
